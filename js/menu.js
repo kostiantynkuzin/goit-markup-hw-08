8c2c7aed0eb2b1f5851aecf0fe1fd2ce30838400
Kostiantyn Kuzin <worldiswonderful2012@gmail.com>
2022-05-31 14:02:29 +0300

    finish2

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,6 @@
     openMenuBtn: document.querySelector('.menu-open-btn'),
     closeMenuBtn: document.querySelector('.menu-close-btn'),
     menu: document.querySelector('.mob-menu'),
-    body: document.querySelector('.body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -11,6 +10,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
-    refs.body.classList.toggle('no - scroll');
+    document.body.classList.toggle('menu-open');
   }
 })();
